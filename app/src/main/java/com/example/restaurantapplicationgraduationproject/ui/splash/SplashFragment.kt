54a494cefa6b72329c94e78117d462408cd202a4
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-08-16 09:57:46 +0300

    Added implemetations, added navigation and created Splash Fragment

---
+++ app/src/main/java/com/example/restaurantapplicationgraduationproject/ui/splash/SplashFragment.kt
@@ -0,0 +1,25 @@
+package com.example.restaurantapplicationgraduationproject.ui.splash
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.restaurantapplicationgraduationproject.R
+
+
+class SplashFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        Handler(Looper.getMainLooper()).postDelayed({
+
+        }, 5000)
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+}
