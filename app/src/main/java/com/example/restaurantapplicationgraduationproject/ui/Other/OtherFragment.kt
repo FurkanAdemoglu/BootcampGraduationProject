128e86fedf754a9de4d37ccd762fc559726508b3
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-08-20 12:29:15 +0300

    Other and register fragment layout coded

---
+++ app/src/main/java/com/example/restaurantapplicationgraduationproject/ui/Other/OtherFragment.kt
@@ -0,0 +1,13 @@
+package com.example.restaurantapplicationgraduationproject.ui.Other
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.restaurantapplicationgraduationproject.R
+
+
+class OtherFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/example/restaurantapplicationgraduationproject/ui/profile/ProfileFragment.kt
@@ -0,0 +1,20 @@
+package com.example.restaurantapplicationgraduationproject.ui.profile
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.restaurantapplicationgraduationproject.R
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class ProfileFragment : Fragment() {
+
+}
